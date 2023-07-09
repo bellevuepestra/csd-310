dfe7cbc40c71d5f937081814493c98bf837fde6c
bellevuepestra <135786016+bellevuepestra@users.noreply.github.com>
2023-07-08 23:28:24 -0400

    Add files via upload

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://admin:admin@cluster0.4eilq7t.mongodb.net/?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
+
+client = MongoClient(uri)
+
+db = client.pytech
+
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+input("\n\n  End of program, press any key to exit... ")
